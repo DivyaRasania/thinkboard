ac57a822f0352858ec8020a151f966a6f05801a5
div <rasaniadivya1@gmail.com>
2026-06-10 00:32:34 -0400

    feat: add rate limiting with Upstash and update server configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,35 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
+// get environment variables
 dotenv.config();
 
+// setup express app and port number
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
+// enable middleware
+app.use(express.json()); // middleware - runs between the request and the response; parses JSON bodies: req.body
 
-app.use(express.json()); //middleware
+// simple middleware eg:
+/*
+ * app.use((req, res, next) => {
+ *     console.log(`Request method is ${req.method} and request URL is ${req.url}`);
+ *     next();
+ * })
+ */
 
+// enable rate limiting
+app.use(rateLimiter);
+
+// setup routes
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+// connect to mongodb and start listening
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on ${PORT}`);
+    });
 });
