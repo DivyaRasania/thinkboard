74c90dc3381fb874aab9273085796f56cd5da25c
div <rasaniadivya1@gmail.com>
2026-06-10 23:10:05 -0400

    feat: add NotesNotFound component and implement note deletion functionality

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,8 +1,26 @@
 import {Link} from "react-router";
+import toast from "react-hot-toast";
 import {PenSquareIcon, Trash2Icon} from "lucide-react";
-import {formatDate} from "../lib/utils.js";
 
-const NoteCard = ({note}) => {
+import {formatDate} from "../lib/utils.js";
+import api from "../lib/axios.js";
+
+const NoteCard = ({note, setNotes}) => {
+    const handleDelete = async (e, id) => {
+        e.preventDefault(); //get rid of the navigation behavior
+
+        if (!window.confirm("Are you sure you want to delete this note?")) return;
+
+        try {
+            await api.delete(`notes/${id}`);
+            setNotes((prev) => prev.filter(note => note._id !== id)); // get rid of the deleted not in HomePage notes state
+            toast.success("Note deleted successfully");
+        } catch (err) {
+            console.error(err);
+            toast.error("Failed to delete note");
+        }
+    };
+
     return (
         <Link to={`/note/${note._id}`}
               className={"card bg-base-100 hover:shadow-lg transition-all " +
@@ -17,7 +35,10 @@ const NoteCard = ({note}) => {
 
                     <div className={"flex items-center gap-1"}>
                         <PenSquareIcon className={"size-4"}/>
-                        <button className={"btn btn-ghost btn-xs text-error"}>
+                        <button className={"btn btn-ghost btn-xs text-error"}
+                                onClick={(e) => {
+                                    handleDelete(e, note._id)
+                                }}>
                             <Trash2Icon className={"size-4"}/>
                         </button>
                     </div>
